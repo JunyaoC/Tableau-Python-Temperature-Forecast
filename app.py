a0ca8535e11048771a30a401043a61373efd09e9
Junyao Chan <junyaoxiandingchan@gmail.com>
2020-06-23 21:07:31 +0800

    -

--- app.py
+++ app.py
@@ -8,16 +8,63 @@ from sklearn import metrics
 
 dataset = pd.read_csv('C:\\Users\\Junyao\\Documents\\GitHub\\Tableau-Python-Temperature-Forecast\\dataset2.csv')
 
-print(dataset.describe())
+print("\nModeling Actual Next Day's Minimum Temperature")
 
-dataset.plot(x='Act_Next_Tmax', y='Act_Next_Tmin', style='o')  
-plt.title('Minimum Temperature vs Maximum Temperature')  
-plt.xlabel('MinTemp')  
-plt.ylabel('MaxTemp')  
-#plt.show()
+antMinMean = dataset['Act_Next_Tmin']
+print("Actual Next Tmin Mean: " + str(antMinMean.mean()))
+print("Mean's 10%: " + str(antMinMean.mean() * 0.1) + "\n")
 
-plt.figure(figsize=(15,10))
-plt.tight_layout()
-seabornInstance.distplot(dataset['Present_Tmax'])
+X = dataset[['Present_Tmax','Present_Tmin','Solar_radiation','DEM','lat','lon','Slope']]
+y = dataset['Act_Next_Tmin'].values.reshape(-1,1)
 
-plt.show()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train) #training the algorithm
+
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'Actual_Next_Tmin': y_test.flatten(), 'Predicted_Next_Tmin': y_pred.flatten()})
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+df1 = df.head(25)
+df1.plot(kind='bar',figsize=(16,10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+tminPredictDF = df
+
+print("\n\nModeling Actual Next Day's Maximum Temperature")
+
+
+antMaxMean = dataset['Act_Next_Tmax']
+print("Actual Next Tmin Mean: " + str(antMaxMean.mean()))
+print("Mean's 10%: " + str(antMaxMean.mean() * 0.1) + "\n")
+
+
+X = dataset[['Present_Tmax','Present_Tmin','Solar_radiation','DEM','lat','lon','Slope']]
+y = dataset['Act_Next_Tmax'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+regressor = LinearRegression()  
+regressor.fit(X_train, y_train) #training the algorithm
+
+y_pred = regressor.predict(X_test)
+df = pd.DataFrame({'Actual_Next_Tmax': y_test.flatten(), 'Predicted_Next_Tmax': y_pred.flatten()})
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+df1 = df.head(25)
+df1.plot(kind='bar',figsize=(16,10))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+
+tmaxPredictDF = df
+
+result = pd.concat([tminPredictDF,tmaxPredictDF],axis=1,sort=False)
+
+result.to_csv("prediction_result.csv")
